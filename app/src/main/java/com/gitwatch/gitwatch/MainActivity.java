0524602c57cc58528d7fa0ba561c0772746074c6
Yannick Wittwer <yannick.wittwer@staempfli.com>
2017-05-23 09:49:14 +0200

    Initial Commit

---
+++ app/src/main/java/com/gitwatch/gitwatch/MainActivity.java
@@ -0,0 +1,49 @@
+package com.gitwatch.gitwatch;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText searchfield;
+    private TextView searchfieldLable;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_user:
+                    searchfield.setHint("Benutzersuche");
+                    searchfieldLable.setText("Nach Benutzer suchen");
+                    return true;
+                case R.id.navigation_repository:
+                    searchfield.setHint("Repositorysuche");
+                    searchfieldLable.setText("Nach Repository suchen");
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        searchfield = (EditText) findViewById(R.id.searchfield);
+        searchfieldLable = (TextView) findViewById(R.id.searchfieldLable);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
