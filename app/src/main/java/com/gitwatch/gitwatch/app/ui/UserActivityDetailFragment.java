a0a45ad2a73bcd2598d3db33b27d6fa39bfec711
Yannick Wittwer <yannick.wittwer@staempfli.com>
2017-05-23 14:22:39 +0200

    Add UserList Activity
    -Add List and Detail View from users

---
+++ app/src/main/java/com/gitwatch/gitwatch/app/ui/UserActivityDetailFragment.java
@@ -0,0 +1,83 @@
+package com.gitwatch.gitwatch.app.ui;
+
+import android.app.Activity;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.gitwatch.gitwatch.R;
+import com.gitwatch.gitwatch.app.ui.dummy.DummyContent;
+import com.gitwatch.gitwatch.core.Domain.Model.User;
+import com.gitwatch.gitwatch.infrastructure.github.Services.UserService;
+
+import org.json.JSONException;
+
+/**
+ * A fragment representing a single UserActivity detail screen.
+ * This fragment is either contained in a {@link UserActivityListActivity}
+ * in two-pane mode (on tablets) or a {@link UserActivityDetailActivity}
+ * on handsets.
+ */
+public class UserActivityDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private User mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public UserActivityDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+
+            String id = getArguments().getString(ARG_ITEM_ID);
+
+            try {
+                mItem = (User) new UserService().getById(Long.parseLong(id));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.name);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.useractivity_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            if(mItem.bio != "null"){
+                ((TextView) rootView.findViewById(R.id.useractivity_detail)).setText(mItem.bio);
+            }
+        }
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/gitwatch/gitwatch/app/ui/UserActivityListActivity.java
@@ -0,0 +1,146 @@
+package com.gitwatch.gitwatch.app.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.gitwatch.gitwatch.R;
+import com.gitwatch.gitwatch.core.Domain.Model.User;
+import com.gitwatch.gitwatch.infrastructure.github.Services.UserService;
+
+import org.json.JSONException;
+
+import java.util.List;
+
+/**
+ * An activity representing a list of Items. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link UserActivityDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class UserActivityListActivity extends AppCompatActivity {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+    private List<User> UserList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_useractivity_list);
+
+        String keyword = getIntent().getStringExtra("keyword");
+        try {
+            UserList = new UserService().getByName(keyword);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        View recyclerView = findViewById(R.id.useractivity_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+
+
+
+        if (findViewById(R.id.useractivity_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(UserList));
+    }
+
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final List<User> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<User> items) {
+            mValues = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.useractivity_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+
+            holder.mContentView.setText(mValues.get(position).name);
+            holder.mScoreView.setText(Double.toString(mValues.get(position).score));
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mTwoPane) {
+                        Bundle arguments = new Bundle();
+                        arguments.putString(UserActivityDetailFragment.ARG_ITEM_ID, Long.toString(holder.mItem.id));
+                        UserActivityDetailFragment fragment = new UserActivityDetailFragment();
+                        fragment.setArguments(arguments);
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.useractivity_detail_container, fragment)
+                                .commit();
+                    } else {
+                        Context context = v.getContext();
+                        Intent intent = new Intent(context, UserActivityDetailActivity.class);
+                        intent.putExtra(UserActivityDetailFragment.ARG_ITEM_ID, Long.toString(holder.mItem.id));
+
+                        context.startActivity(intent);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mContentView;
+            public final TextView mScoreView;
+            public User mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mContentView = (TextView) view.findViewById(R.id.content);
+                mScoreView = (TextView) view.findViewById(R.id.score);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mContentView.getText() + "'";
+            }
+        }
+    }
+}
